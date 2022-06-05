2a222a470ae6ecb568b9218a4fd6be39f56e46d4
Ayush Pawar <103307248+Ayushpwr27@users.noreply.github.com>
2022-06-05 23:02:03 +0530

    Update index.js

--- Blackjack Cards Game/index.js
+++ Blackjack Cards Game/index.js
@@ -7,20 +7,22 @@ let sumEl = document.querySelector("#sum-el")
 let cardEl = document.querySelector("#card-el")
 let messageEl = document.querySelector("#message-el")
 
+// display player details
 let player = {
     name : "Ayush",
     chips : 150
 }
-
 let playerEl = document.querySelector("#player-el")
 playerEl.textContent = player.name + ": $" + player.chips
 
+
+// function for drawing random cards
 function getRandomeCard(){
-    let randomNum = Math.floor(Math.random()*13) +1
-    if (randomNum > 10 ) {
-        return 10
+    let randomNum = Math.floor(Math.random()*13) +1 // since there are 13 different cards in a deck including ace, king, queen ,and jack and others cards which have value accordingly  
+    if (randomNum > 10 ) { // in blackjack king, queen ,and jack is treated as 10
+        return 10  
     }
-    else if (randomNum === 1){
+    else if (randomNum === 1){ //in blackjack ace is treated as 1 or 11 which is chosen by the player 
         return 11
     }
     else{
@@ -28,6 +30,7 @@ function getRandomeCard(){
     }
 }
 
+//Initiates the game on clicking start game button 
 function startGame(){
     isAlive = true
     let firstCard = getRandomeCard()
@@ -37,14 +40,13 @@ function startGame(){
     renderGame()
 }
 
-
-
+// renders card on the screen
 function renderGame(){
     cardEl.textContent = "Cards: "
     for(let i=0; i<cards.length ; i++){
         cardEl.textContent += cards[i] + " "
     }
-
+    // rules
     sumEl.textContent = "Sum: " + sum 
     if (sum <= 20) {
         message = "do you want to draw a card"
@@ -60,6 +62,7 @@ function renderGame(){
     messageEl.textContent = message
 }
 
+// draw new card
 function newCard(){
     
     if (isAlive === true && hasBlackJack === false) {
